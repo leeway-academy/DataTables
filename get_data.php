c361e4127dfa28d602595430f2004a48a7c6e523
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2021-04-28 10:49:01 -0300

    Using server side processing

--- get_data.php
+++ get_data.php
@@ -6,7 +6,35 @@ try {
     die($exception->getMessage());
 }
 
-$sql = "SELECT * FROM products";
+$orderBy = " ORDER BY ";
+foreach ($_GET['order'] as $order) {
+    $orderBy .= $order['column'] + 1 . " {$order['dir']}, ";
+}
+
+$orderBy = substr($orderBy, 0, -2);
+$where = '';
+
+$columns = $_GET['columns'];
+$fields = ['id', 'name', 'price'];
+$where = '';
+
+foreach ($columns as $k => $column) {
+    if ($search = $column['search']['value']) {
+        $where .= $fields[$k].' = '.$search.' AND ';
+    }
+}
+
+$where = substr($where, 0, -5);
+$length = $_GET['length'];
+$start = $_GET['start'];
+
+$countSql = "SELECT count(id) as Total FROM products";
+$countSt = $conn
+    ->query($countSql);
+
+$total = $countSt->fetch()['Total'];
+
+$sql = "SELECT * FROM products ".($where ?? "WHERE $where ")."$orderBy LIMIT $length OFFSET $start";
 $st = $conn
     ->query($sql);
 
@@ -15,6 +43,8 @@ if ($st) {
 
     echo json_encode([
         'data' => $rs,
+        'recordsTotal' => $total,
+        'recordsFiltered' => count($rs),
     ]);
 } else {
     var_dump($conn->errorInfo());
